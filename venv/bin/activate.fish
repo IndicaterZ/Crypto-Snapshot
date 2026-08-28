ccf786f643f13769d3ac7caef4237b45f54b28ff
supakit-sakkullapat <supakit.sak@kkumail.com>
2026-08-28 22:09:47 +0700

    fix: change cron schedule to avoid top-of-the-hour delays

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/s.s/.gemini/antigravity/scratch/crypto-snapshot/venv"
+set -gx VIRTUAL_ENV "/Users/s.s/Desktop/crypto-snapshot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
